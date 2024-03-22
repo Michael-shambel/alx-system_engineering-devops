8b71934d950f0880d5e5c655f8c05721a684f796
Michael-shambel <Michaelshambel10@gmail.com>
2024-03-22 13:58:21 -0700

    task 1

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+# Using Puppet, install flask from pip3.
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+}
