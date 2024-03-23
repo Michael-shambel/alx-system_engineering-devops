b5da549ed5d42dac8112e21feab70bcefec42d04
Michael-shambel <Michaelshambel10@gmail.com>
2024-03-23 00:32:10 -0700

    task 1 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,6 +2,6 @@
 # install flask from pip3 Version must be 2.1.0
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.0.3',
   unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
