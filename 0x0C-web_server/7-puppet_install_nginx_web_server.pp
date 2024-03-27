541ae6aab3a441af5bf852b33ac90748222bbd1f
michael-shambel <Michaelshambel10@gmail.com>
2024-03-27 11:47:16 -0700

    task 100ee

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,23 +1,28 @@
 #script used to install nginx using puppet
-exec { 'update system':
-	command => '/usr/bin/apt-get update',
+package {'nginx':
+	ensure => 'present',
 }
 
-package { 'nginx':
-	ensure => 'installed',
-	require => Exec['update system']
+exec {'update system':
+        command  => 'sudo apt-get update',
+        provider => shell,
 }
 
-file {'/var/www/html/index.html':
-	content => 'Hello World!'
+exec {'install system':
+	command  => 'sudo apt-get -y install nginx',
+	provider => shell,
 }
 
-exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-	provider => 'shell'
+exec {'Hello World!':
+	command  => 'echo "Hello World!" | sudo dd status=none of=/var/www/html/index.html',
+	provider => shell,
 }
 
-service {'nginx':
-	ensure => runnig,
-	require => package['nginx']
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
+}
+
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
