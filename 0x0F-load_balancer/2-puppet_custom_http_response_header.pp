1048c743c8c5d21b729431534b961afe57944842
michael-shambel <Michaelshambel10@gmail.com>
2024-04-11 04:36:29 -0700

    task 100

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,50 +6,17 @@ exec {'update':
   before   => Exec['install Nginx'],
 }
 
-exec {'install Nginx':
+-> package {'nginx':
+  ensure => present,
+}
+-> file_line { 'header line':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "	location / {
+  add_header X-Served-By ${hostname};",
+  match  => '^\tlocation / {',
+}
+-> exec { 'restart service':
+  command  => 'sudo service nginx restart',
   provider => shell,
-  command  => 'sudo apt-get -y install nginx',
-  before   => Exec['add_header'],
-}
-
-exec { 'allow_nginx_http':
-  command => '/usr/sbin/ufw allow "Nginx HTTP"',
-  path    => ['/usr/sbin', '/usr/bin', '/bin'],
-  unless  => '/usr/sbin/ufw status | grep "Nginx HTTP" | grep ALLOW',
-  require => Package['ufw'], # Ensure the ufw package is installed before executing the command
-}
-
-$hostname = $facts['networking']['hostname']
-$nginx_config = "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-    server_name _;
-    add_header X-Served-By ${hostname};
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-    if (\$request_filename ~ redirect_me){
-        rewrite ^ https://youtube.com permanent;
-    }
-    error_page 404 /404.html;
-    location = /404.html {
-        internal;
-    }
-}"
-
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => $nginx_config,
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
 }
