e594b2ec8cd736566084a4f9414ab3fa70a0946f
michael-shambel <Michaelshambel10@gmail.com>
2024-04-24 13:51:56 -0700

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -30,7 +30,7 @@ def display_progress(employee_id):
     print("Employee {} is done with tasks({}/{}):".format(
           employee_name, num_completed_task, total_task))
     for task in completed_task:
-        print("\t{}".format(task['title'].expandtabs(1)))
+        print("\t{}".format(task['title']))
 
 
 if __name__ == "__main__":
