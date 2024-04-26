cfb2f4b932b4c17f78fb0d4f3489c7c05083aab0
michael-shambel <Michaelshambel10@gmail.com>
2024-04-26 04:30:00 -0700

    task 12

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -24,11 +24,15 @@ def display_progress(employee_id):
     todo_info = fetch_user(employee_id)
     todo_list = fetch_todo(employee_id)
     employee_name = todo_info['username']
-    json_data = {str(employee_id): [{"task": task['title'], "completed": task['completed'], "username": employee_name} for task in todo_list]}
+    json_data = {str(employee_id):
+                 [{"task": task['title'], "completed":
+                  task['completed'], "username": employee_name}
+                  for task in todo_list]}
     json_file_name = "{}.json".format(employee_id)
 
     with open(json_file_name, 'w') as jsonfile:
         json.dump(json_data, jsonfile)
+
 
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
