b510cb39587fd18545d8456a2c61b1dcbf85d7e4
michael-shambel <Michaelshambel10@gmail.com>
2024-04-26 05:11:31 -0700

    task final

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,8 +4,8 @@ Using what you did in the task #0, extend your Python script
 to export data in the JSON format.
 """
 
-import requests
 import json
+import requests
 import sys
 
 if __name__ == "__main__":
