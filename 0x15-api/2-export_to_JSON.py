b11ec812787aa4ea71a3c6e350729c6a5096c153
michael-shambel <Michaelshambel10@gmail.com>
2024-04-26 04:20:18 -0700

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -20,21 +20,29 @@ def fetch_todo(employee_id):
     return resp.json()
 
 
-def export_to_json(employee_id, todo_info, todo_list):
-    employee_name = todo_info['name']
-    tasks = [{'task': task['title'], 'completed': task['completed'],
-             'username': employee_name} for task in todo_list]
-    data = {str(employee_id): tasks}
-    filename = "{}.json".format(employee_id)
-    with open(filename, mode='w') as file:
-        json.dump(data, file)
+def display_progress(employee_id):
+    todo_info = fetch_user(employee_id)
+    todo_list = fetch_todo(employee_id)
+    employee_name = todo_info['username']
+    json_data = {str(employee_id): [{"task": task['title'], "completed": task['completed'], "username": employee_name} for task in todo_list]}
+    json_file_name = "{}.json".format(employee_id)
+
+    with open(json_file_name, 'w') as jsonfile:
+        json.dump(json_data, jsonfile)
+
+#def export_to_json(employee_id, todo_info, todo_list):
+    #employee_name = todo_info['name']
+    #tasks = [{'task': task['title'], 'completed': task['completed'],
+             #'username': employee_name} for task in todo_list]
+    #data = {str(employee_id): tasks}
+    #filename = "{}.json".format(employee_id)
+    #with open(filename, mode='w') as file:
+        #json.dump(data, file)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
     employee_id = int(sys.argv[1])
-    todo_info = fetch_user(employee_id)
-    todo_list = fetch_todo(employee_id)
-    export_to_json(employee_id, todo_info, todo_list)
+    display_progress(employee_id)
+    #todo_info = fetch_user(employee_id)
+    #todo_list = fetch_todo(employee_id)
+    #export_to_json(employee_id, todo_info, todo_list)
