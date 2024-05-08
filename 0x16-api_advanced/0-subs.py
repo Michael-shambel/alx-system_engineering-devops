741311180d18f3ef5f485edc63c1e2cf253799c0
michael-shambel <Michaelshambel10@gmail.com>
2024-05-08 03:21:49 -0700

    task 0 u2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,8 @@ import requests
 def number_of_subscribers(subreddit):
     """return number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    resp = requests.get(url)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    resp = requests.get(url, headers=headers)
     data = resp.json()
     if resp.status_code == 200:
         subscribers = data['data']['subscribers']
