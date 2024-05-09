792a2fb0c413c002e82818972a9ae54ddae0adec
michael-shambel <Michaelshambel10@gmail.com>
2024-05-09 12:53:50 -0700

    task 1-03

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,31 +6,32 @@ delimited by spaces.
 """
 import requests
 
+def count_words(subreddit, word_list, after=None, word_count=None):
+    if word_count is None:
+        word_count = {}
 
-def count_words(subreddit, word_list, after=None, word_count=[]):
     if not word_list:
         return
+    
     if after:
-        url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
-                                                                     after)
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
     else:
-        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    resp.requests.get(url)
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+
+    resp = requests.get(url)
     if resp.status_code == 200:
         data = resp.json()
         posts = data['data']['children']
         for post in posts:
-            title = post['data']['title']
-            title_lower = title.lower()
+            title = post['data']['title'].lower()
             for word in word_list:
-                if word.lower() in title_lower.split():
-                    word_count[
-                        word.lower()] = word_count.get(word.lower(), 0) + 1
+                if word.lower() in title:
+                    word_count[word.lower()] = word_count.get(word.lower(), 0) + 1
+
         after = data['data'].get('after')
         if after:
             return count_words(subreddit, word_list, after, word_count)
         else:
-            sorted_word_count = sorted(word_count.items(), key=lambda x:
-                                       (-x[1], x[0]))
+            sorted_word_count = sorted(word_count.items(), key=lambda x: (-x[1], x[0]))
             for word, count in sorted_word_count:
-                print("{}: {}".format(word, count))
+                print(f"{word}: {count}")
